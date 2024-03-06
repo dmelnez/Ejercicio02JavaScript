3500487c75014bb33bfa3bdb55850b394320877b
David Melendez <145343465+dmelnez@users.noreply.github.com>
2024-03-06 19:24:45 +0100

    Add files via upload

---
+++ edu.JavaScript02.LM/src/main/webapp/javaScript02.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+var fecha=
+	{
+		dia: 6,
+		mes: 3,
+		anyo: 2024
+	}
+
+	
+let calendario=
+	{
+		
+		dia: 6,
+		mes: "Marzo",
+		anyo: 2024	
+	}
+
+if(fecha.dia == calendario.dia && fecha.mes == calendario.mes && fecha.anyo == calendario.anyo){
+	
+	alert("Es Hoy");
+	
+}
+
+else{
+	
+	alert("Aun Falta")
+	
+}
